1181738ad168374f5d2751219359ef0ad09318f1
Autumn <543492941@qq.com>
2026-01-04 21:33:13 +0800

    second

--- packages/play/src/App.vue
+++ packages/play/src/App.vue
@@ -6,13 +6,6 @@
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
